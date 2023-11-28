1768d1556765d5b0e717ab602d0d67ace6b1f2ae
Kiguradze <bkiguradze@gmail.com>
2023-11-28 10:32:46 +0400

    add product page header

--- src/pages/productPage/Product.jsx
+++ src/pages/productPage/Product.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../../components/header/Header";
+import productHeroImage from "../../assets/productHeroImage.png";
 
 const Product = () => {
   return (
     <>
-      <Header />
+      <Header
+        title="Help your team to focus on priority tasks"
+        desk="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque ."
+        image={productHeroImage}
+      />
     </>
   );
 };
