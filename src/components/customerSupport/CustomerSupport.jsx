76f684c800d99bdc1e1d36e6aa73f120e1e4aa2a
Kiguradze <bkiguradze@gmail.com>
2023-11-28 11:24:47 +0400

    add customer support section

---
+++ src/components/customerSupport/CustomerSupport.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./customerSupport.css";
+import analiticsSvg from "../../assets/customerSupportAnalitics.svg";
+import bestQualitySvg from "../../assets/customerSupportBestQuality.svg";
+import moreClicksSvg from "../../assets/customerSupportMoreClicks.svg";
+
+const CustomerSupport = () => {
+  return (
+    <section className="customer-support">
+      <div className="container">
+        <h1></h1>
+        <div className="customer-support-grid">
+          <div className="customer-support-card">
+            <div className="logo-background">
+              <img src={analiticsSvg} alt="" />
+            </div>
+            <h2>Analytics</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum vitae nunc vitae neque
+            </p>
+          </div>
+          <div className="customer-support-card">
+            <div className="logo-background">
+              <img src={bestQualitySvg} alt="" />
+            </div>
+            <h2>Best Quality</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum vitae nunc vitae neque
+            </p>
+          </div>
+          <div className="customer-support-card">
+            <div className="logo-background">
+              <img src={moreClicksSvg} alt="" />
+            </div>
+            <h2>More Clicks</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum vitae nunc vitae neque
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CustomerSupport;
--- src/pages/productPage/Product.jsx
+++ src/pages/productPage/Product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import productHeroImage from "../../assets/productHeroImage.png";
+import CustomerSupport from "../../components/customerSupport/CustomerSupport";
 
 const Product = () => {
   return (
@@ -10,6 +11,7 @@ const Product = () => {
         desk="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae nunc vitae neque ."
         image={productHeroImage}
       />
+      <CustomerSupport />
     </>
   );
 };
