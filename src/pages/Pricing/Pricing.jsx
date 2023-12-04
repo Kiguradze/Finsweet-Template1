b4c1b16715092a373eba4d8b503f10cb3b5396a2
Kiguradze <bkiguradze@gmail.com>
2023-12-04 11:42:36 +0400

    update price cards css

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import PriceCard from "../../components/priceCard/PriceCard";
+import Faq from "../../components/FAQ/Faq";
 import "./pricing.css";
 import data from "../../data/priceCardsData/priceCardsData";
 
@@ -26,6 +27,7 @@ const Pricing = () => {
           ))}
         </div>
       </div>
+      <Faq />
     </>
   );
 };
