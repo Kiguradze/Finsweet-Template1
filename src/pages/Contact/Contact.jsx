01000edb7e791f498c8fc74134fcd53d62cc9c4c
Kiguradze <bkiguradze@gmail.com>
2023-12-09 19:48:27 +0400

    update contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,14 +1,25 @@
-import React from "react";
-import Nav from "../../components/header/nav/Nav";
+import { useState } from "react";
 import "./contact.css";
+import Menu from "../../components/menu/Menu";
+import Nav from "../../components/header/nav/Nav";
 import ContactSection from "./ContactSection";
+import Footer from "../../components/footer/Footer";
 
 const Contact = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
   return (
-    <div className="container">
-      <Nav />
+    <>
+      <Menu isActive={isActive} handleClick={handleClick} />
+      <div className="container">
+        <Nav isActive={isActive} handleClick={handleClick} />
+      </div>
       <ContactSection />
-    </div>
+      <Footer />
+    </>
   );
 };
 
--- src/pages/Contact/ContactSection.jsx
+++ src/pages/Contact/ContactSection.jsx
@@ -5,23 +5,25 @@ import shape from "../../assets/contactShape.png";
 const ContactSection = () => {
   return (
     <section className="contact-section">
-      <div className="contact-info">
-        <img src={shape} alt="" />
-        <h1>Get in touch with us</h1>
-        <p>398 11th St, San Francisco</p>
-      </div>
-
-      <form className="contact-form" action="">
-        <h1>We will get back to you as soon as possible.</h1>
-        <div className="name">
-          <input type="text" placeholder="FirstName" />
-          <input type="text" placeholder="LastName" />
+      <div className="container">
+        <div className="contact-info">
+          <img src={shape} alt="" />
+          <h1>Get in touch with us</h1>
+          <p>398 11th St, San Francisco</p>
         </div>
-        <input className="email" type="email" placeholder="Work email" />
-        <input className="subject" type="text" placeholder="Subject" />
-        <textarea className="text-area" placeholder="Message" />
-        <button>Submit</button>
-      </form>
+
+        <form className="contact-form" action="">
+          <h1>We will get back to you as soon as possible.</h1>
+          <div className="name">
+            <input type="text" placeholder="FirstName" />
+            <input type="text" placeholder="LastName" />
+          </div>
+          <input className="email" type="email" placeholder="Work email" />
+          <input className="subject" type="text" placeholder="Subject" />
+          <textarea className="text-area" placeholder="Message" />
+          <button>Submit</button>
+        </form>
+      </div>
     </section>
   );
 };
