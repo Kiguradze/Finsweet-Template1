764799c9866d88620823b2d2173ad7bc48b768c4
Kiguradze <bkiguradze@gmail.com>
2023-12-10 21:23:32 +0400

    update browserRouter to hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/homePage/Home";
 import Product from "./pages/productPage/Product";
 import Pricing from "./pages/Pricing/Pricing";
