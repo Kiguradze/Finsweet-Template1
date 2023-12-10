1e3632db81070cb1f0fbd188d64b2ddf74fb5b77
Kiguradze <bkiguradze@gmail.com>
2023-12-10 19:09:54 +0400

    add privacy page

--- src/pages/Privacy/Privacy.jsx
+++ src/pages/Privacy/Privacy.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import "./privacy.css";
 import Menu from "../../components/menu/Menu";
 import Nav from "../../components/header/nav/Nav";
 import PrivacyContent from "./PrivacyContent";
+import Footer from "../../components/footer/Footer";
 
 const Privacy = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <>
-      <Menu />
+      <Menu isActive={isActive} handleClick={handleClick} />
       <div className="container">
-        <Nav />
+        <Nav isActive={isActive} handleClick={handleClick} />
         <PrivacyContent />
       </div>
+      <Footer />
     </>
   );
 };
